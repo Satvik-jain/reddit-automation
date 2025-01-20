bdffc2aa961c8a9f8d22ba4d5b6a3cc747ca7179
Satvik Jain <satvik.jain.kht@gmail.com>
2025-01-20 18:22:59 +0530

    reddit instance created

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-import praw 
+import praw
+
+from dotenv import load_dotenv
+load_dotenv()
+
+import os
+CLIENT_ID = os.getenv("CLIENT_ID")
+SECRET_ID = os.getenv("SECRET_ID")
+PASSWORD = os.getenv("PASSWORD")
+
+reddit = praw.Reddit(
+    client_id=CLIENT_ID,
+    client_secret=SECRET_ID,
+    password=PASSWORD,
+    user_agent="reddit_assign by u/Narrow_Block_8755",
+    username="Narrow_Block_8755",
+)
+
+print(reddit.read_only)
+
+# reddit.read_only = True
+
