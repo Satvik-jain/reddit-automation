508e6a63cd74242f74b8da4daf4095680b6ed6bb
Satvik Jain <satvik.jain.kht@gmail.com>
2025-01-21 00:35:03 +0530

    added submission and thread instances

--- reddit_instance.py
+++ reddit_instance.py
@@ -1,5 +1,3 @@
-import praw
-
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -8,6 +6,7 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 SECRET_ID = os.getenv("SECRET_ID")
 PASSWORD = os.getenv("PASSWORD")
 
+import praw
 reddit = praw.Reddit(
     client_id=CLIENT_ID,
     client_secret=SECRET_ID,
@@ -19,8 +18,21 @@ reddit = praw.Reddit(
 # print(reddit.read_only)
 
 from llm_groq import CallGroq
+def posting(username = "u_Narrow_Block_8755"):
+    subreddit = reddit.subreddit(username)
+    subreddit.submit(title="Daily Blog", selftext=CallGroq())
 
-subreddit = reddit.subreddit("u_Narrow_Block_8755")
-subreddit.submit(title="AI Generated Content", selftext=CallGroq())
 # reddit.read_only = True
 
+from llm_groq import CommentGroq
+def commenting(url):
+    submission = reddit.submission(url = url)
+    reply = CommentGroq(post = submission.selftext)
+    submission.reply(reply)
+
+def thread(url):
+    comment = reddit.comment(url = url)
+    reply = CommentGroq(post = comment.body)
+    comment.reply(reply)
+
+# print(commenting("https://www.reddit.com/user/Narrow_Block_8755/comments/1i5tq4m/ai_generated_content/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button"))
